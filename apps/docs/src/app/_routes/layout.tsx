39756c571f13f03e42bd925f62b9885da0f8ef3e
dilane3 <komboudilane125@gmail.com>
2026-03-28 13:56:46 +0100

    chore: adding vercel analytics

--- apps/docs/src/app/_routes/layout.tsx
+++ apps/docs/src/app/_routes/layout.tsx
@@ -3,12 +3,15 @@ import { Navbar } from '@/components/common/layout/navbar';
 import { useTheme } from '@rasenganjs/theme';
 import { cn } from '@/lib/utils';
 import Footer from '@/components/common/layout/footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const RootLayout: LayoutComponent = () => {
   const { isDark } = useTheme();
 
   return (
     <section className={cn('w-full h-full text-foreground font-raleway', isDark && "dark")}>
+      <Analytics framework='rasengan.js' />
+
       <Navbar />
 
       <main className='w-full h-full bg-background'>
