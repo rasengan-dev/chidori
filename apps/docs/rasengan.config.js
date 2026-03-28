fb736fcb2cbb6c8da6f433988f4643b9038598cc
dilane3 <komboudilane125@gmail.com>
2026-03-28 13:43:53 +0100

    chore: add @rasenganjs/vercel adapter

--- apps/docs/rasengan.config.js
+++ apps/docs/rasengan.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'rasengan';
 import { rasengan } from 'rasengan/plugin';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@rasenganjs/mdx/plugin';
+import { configure } from "@rasenganjs/vercel";
 
 export default defineConfig(async () => {
   return {
+    prerender: true,
     vite: {
-      plugins: [tailwindcss(), rasengan({}), mdx({ code: { keepBackground: false } })],
+      plugins: [tailwindcss(), rasengan({ adapter: configure({}) }), mdx({ code: { keepBackground: false } })],
     },
   };
 });
